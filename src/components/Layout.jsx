abb71acbb0f1b3dd54b6f659fe45acf66f608b07
Remco Hoeneveld <remco.hoeneveld@iodigital.com>
2023-02-16 01:15:24 +0100

    init

---
+++ src/components/Layout.jsx
@@ -0,0 +1,164 @@
+import { Fragment, useId, useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { AudioPlayer } from '@/components/player/AudioPlayer'
+import posterImage from '@/images/poster.png'
+
+function randomBetween(min, max, seed = 1) {
+  return () => {
+    let rand = Math.sin(seed++) * 10000
+    rand = rand - Math.floor(rand)
+    return Math.floor(rand * (max - min + 1) + min)
+  }
+}
+
+function SpotifyIcon(props) {
+  return (
+  <svg viewBox="0 0 2931 2931" width="32" height="32" {...props}>
+    <path className="st0"
+          d="M1465.5 0C656.1 0 0 656.1 0 1465.5S656.1 2931 1465.5 2931 2931 2274.9 2931 1465.5C2931 656.2 2274.9.1 1465.5 0zm672.1 2113.6c-26.3 43.2-82.6 56.7-125.6 30.4-344.1-210.3-777.3-257.8-1287.4-141.3-49.2 11.3-98.2-19.5-109.4-68.7-11.3-49.2 19.4-98.2 68.7-109.4C1242.1 1697.1 1721 1752 2107.3 1988c43 26.5 56.7 82.6 30.3 125.6zm179.3-398.9c-33.1 53.8-103.5 70.6-157.2 37.6-393.8-242.1-994.4-312.2-1460.3-170.8-60.4 18.3-124.2-15.8-142.6-76.1-18.2-60.4 15.9-124.1 76.2-142.5 532.2-161.5 1193.9-83.3 1646.2 194.7 53.8 33.1 70.8 103.4 37.7 157.1zm15.4-415.6c-472.4-280.5-1251.6-306.3-1702.6-169.5-72.4 22-149-18.9-170.9-91.3-21.9-72.4 18.9-149 91.4-171 517.7-157.1 1378.2-126.8 1922 196 65.1 38.7 86.5 122.8 47.9 187.8-38.5 65.2-122.8 86.7-187.8 48z"/>
+  </svg>
+  )
+}
+
+
+function YoutubeIcon(props) {
+  return (
+    <svg width="32" height="32" viewBox="5.368 13.434 53.9 37.855" {...props}>
+      <path fill="#FFF"
+            d="M41.272 31.81c-4.942-2.641-9.674-5.069-14.511-7.604v15.165c5.09-2.767 10.455-5.301 14.532-7.561h-.021z"/>
+      <path fill="#E8E0E0"
+            d="M41.272 31.81c-4.942-2.641-14.511-7.604-14.511-7.604l12.758 8.575c.001 0-2.324 1.289 1.753-.971z"/>
+      <path
+            d="M27.691 51.242c-10.265-.189-13.771-.359-15.926-.803-1.458-.295-2.725-.95-3.654-1.9-.718-.719-1.289-1.816-1.732-3.338-.38-1.268-.528-2.323-.739-4.9-.323-5.816-.4-10.571 0-15.884.33-2.934.49-6.417 2.682-8.449 1.035-.951 2.239-1.563 3.591-1.816 2.112-.401 11.11-.718 20.425-.718 9.294 0 18.312.317 20.426.718 1.689.317 3.273 1.267 4.203 2.492 2 3.146 2.035 7.058 2.238 10.118.084 1.458.084 9.737 0 11.195-.316 4.836-.57 6.547-1.288 8.321-.444 1.12-.823 1.711-1.479 2.366a7.085 7.085 0 0 1-3.76 1.922c-8.883.668-16.426.813-24.987.676zM41.294 31.81c-4.942-2.641-9.674-5.09-14.511-7.625v15.166c5.09-2.767 10.456-5.302 14.532-7.562l-.021.021z"/>
+    </svg>
+  )
+}
+
+function FacebookIcon(props) {
+  return (
+    <svg height="32" viewBox="126.445 2.281 589 589" width="32" {...props}>
+      <circle cx="420.945" cy="296.781" r="294.5"/><path d="m516.704 92.677h-65.239c-38.715 0-81.777 16.283-81.777 72.402.189 19.554 0 38.281 0 59.357h-44.788v71.271h46.174v205.177h84.847v-206.531h56.002l5.067-70.117h-62.531s.14-31.191 0-40.249c0-22.177 23.076-20.907 24.464-20.907 10.981 0 32.332.032 37.813 0v-70.403z" fill="#fff"/>
+    </svg>
+  )
+}
+
+function TiktokIcon(props) {
+  return (
+    <svg viewBox="4 4 42 42" width="32" height="32" {...props}>
+      <path
+        d="M41 4H9C6.243 4 4 6.243 4 9v32c0 2.757 2.243 5 5 5h32c2.757 0 5-2.243 5-5V9c0-2.757-2.243-5-5-5m-3.994 18.323a7.482 7.482 0 0 1-.69.035 7.492 7.492 0 0 1-6.269-3.388v11.537a8.527 8.527 0 1 1-8.527-8.527c.178 0 .352.016.527.027v4.202c-.175-.021-.347-.053-.527-.053a4.351 4.351 0 1 0 0 8.704c2.404 0 4.527-1.894 4.527-4.298l.042-19.594h4.02a7.488 7.488 0 0 0 6.901 6.685v4.67"/>
+    </svg>
+  )
+}
+
+function PersonIcon(props) {
+  return (
+    <svg aria-hidden="true" viewBox="0 0 11 12" {...props}>
+      <path d="M5.019 5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Zm3.29 7c1.175 0 2.12-1.046 1.567-2.083A5.5 5.5 0 0 0 5.019 7 5.5 5.5 0 0 0 .162 9.917C-.39 10.954.554 12 1.73 12h6.578Z" />
+    </svg>
+  )
+}
+
+export function Layout({ children }) {
+  let hosts = ['Remco Hoeneveld', 'Jisvy Djelan', 'Gyan Bissumbhar']
+
+  return (
+    <>
+      <header className="lg:bg-brand-blue lg:fixed lg:inset-y-0 lg:left-0 lg:flex lg:w-112 lg:items-start lg:overflow-y-auto xl:w-120">
+        <div className="hidden lg:sticky lg:top-0 lg:flex lg:w-16 lg:flex-none lg:items-center lg:whitespace-nowrap lg:py-12 lg:text-sm lg:leading-7 lg:[writing-mode:vertical-rl]">
+          <span className="font-mono text-white">Hosted by</span>
+          <span className="mt-6 flex gap-6 font-bold text-white">
+            {hosts.map((host, hostIndex) => (
+              <Fragment key={host}>
+                {hostIndex !== 0 && (
+                  <span aria-hidden="true" className="text-white">
+                    /
+                  </span>
+                )}
+                {host}
+              </Fragment>
+            ))}
+          </span>
+        </div>
+        <div className="relative z-10 mx-auto px-4 pb-4 pt-10 sm:px-6 md:max-w-2xl md:px-4 lg:min-h-full lg:flex-auto lg:border-x lg:border-slate-200 lg:py-12 bg-white lg:px-8 xl:px-12">
+          <Link
+            href="/"
+            className="relative mx-auto lg:ml-0 block w-48 overflow-hidden rounded-lg bg-slate-200 shadow-xl shadow-slate-200 sm:w-64 sm:rounded-xl lg:w-48 lg:rounded-2xl"
+            aria-label="Homepage"
+          >
+            <Image
+              className="w-full"
+              src={posterImage}
+              alt=""
+              sizes="(min-width: 1024px) 20rem, (min-width: 640px) 16rem, 12rem"
+              priority
+            />
+            <div className="absolute inset-0 rounded-lg ring-1 ring-inset ring-black/10 sm:rounded-xl lg:rounded-2xl" />
+          </Link>
+          <div className="mt-10 text-center lg:mt-12 lg:text-left">
+            <p className="text-xl font-bold text-slate-900">
+              <Link href="/">Nawerk Podcast</Link>
+            </p>
+            <p className="mt-3 text-lg font-medium leading-8 text-slate-700">
+              Nawerk, de podcast voor na werk. Verschillende gastprekers vertellen over hun passie in hun werk, wat ze doen en hoe ze daar een verschil maken.
+            </p>
+          </div>
+          <section className="mt-10 lg:mt-12">
+            <div className="h-px bg-gradient-to-r from-slate-200/0 via-slate-200 to-slate-200/0 lg:hidden" />
+            <ul
+              role="list"
+              className="mt-4 flex justify-center gap-10 text-base font-medium leading-7 text-slate-700 sm:gap-8 lg:flex-col lg:gap-4"
+            >
+              {[
+                ['Spotify', SpotifyIcon, 'https://open.spotify.com/show/3pdvPZ5KHCAqG4XIMfGzDn'],
+                ['Youtube', YoutubeIcon, 'https://www.youtube.com/@nawerk'],
+                ['Tiktok', TiktokIcon, 'https://www.tiktok.com/@nawerk.podcast'],
+                ['Facebook', FacebookIcon, 'https://www.facebook.com/Nawerk.Podcast/'],
+              ].map(([label, Icon, Url]) => (
+                <li key={label} className="flex">
+                  <Link
+                    href={Url}
+                    className="group flex items-center"
+                    aria-label={label}
+                  >
+                    <Icon className="h-8 w-8 fill-brand-blue group-hover:fill-brand-blue-900" />
+                    <span className="hidden sm:ml-3 sm:block">{label}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      </header>
+      <main className="border-t border-slate-200 lg:relative lg:mb-28 lg:ml-112 lg:border-t-0 xl:ml-120">
+        <div className="relative">{children}</div>
+      </main>
+      <footer className="border-t border-slate-200 bg-slate-50 py-10 pb-40 sm:py-16 sm:pb-32 lg:hidden">
+        <div className="mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4">
+          <h2 className="mt-8 flex items-center font-mono text-sm font-medium leading-7 text-slate-900">
+            <PersonIcon className="h-3 w-auto fill-slate-300" />
+            <span className="ml-2.5">Hosted by</span>
+          </h2>
+          <div className="mt-2 flex gap-6 text-sm font-bold leading-7 text-white">
+            {hosts.map((host, hostIndex) => (
+              <Fragment key={host}>
+                {hostIndex !== 0 && (
+                  <span aria-hidden="true" className="text-white">
+                    /
+                  </span>
+                )}
+                {host}
+              </Fragment>
+            ))}
+          </div>
+        </div>
+      </footer>
+      <div className="fixed inset-x-0 bottom-0 z-10 lg:left-112 xl:left-120">
+        <AudioPlayer />
+      </div>
+    </>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Satoshi', ...defaultTheme.fontFamily.sans],
+      },
+      spacing: {
+        18: '4.5rem',
+        112: '28rem',
+        120: '30rem',
+      },
+      colors: {
+        'brand-blue': {
+          DEFAULT: '#304B75',
+          '50': '#FFFFFF',
+          '100': '#FFFFFF',
+          '200': '#FFFFFF',
+          '300': '#F0F7FC',
+          '400': '#CEE3F6',
+          '500': '#ABCFF0',
+          '600': '#ABCCED',
+          '700': '#4C99DF',
+          '800': '#257DCE',
+          '900': '#257DCE'
+        },
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+  ],
+}
